b5323b0e56a8a62aabb47e7c48998a97354d83e0
ElGatoGamer45 <79714006+ElGatoGamer45@users.noreply.github.com>
2022-05-16 15:23:14 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+function valor_1() {
+    document.getElementById('c2').style.display = 'block';
+    document.getElementById('curacion').style.display = 'none';
+}
+
+function valor_esconde() {
+    document.getElementById('c2').style.display = 'none';
+    document.getElementById('curacion').style.display = 'block';
+}
+function valor_2() {
+    document.getElementById('v2').style.display = 'block';
+    document.getElementById('veneno').style.display = 'none';
+}
+
+function valor_esconde2() {
+    document.getElementById('v2').style.display = 'none';
+    document.getElementById('veneno').style.display = 'block';
+}
