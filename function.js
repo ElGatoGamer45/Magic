be0e2722430f11b26ffc505c1d572e2d3888016f
ElGatoGamer45 <79714006+ElGatoGamer45@users.noreply.github.com>
2022-05-16 16:26:57 -0500

    Update function.js

--- function.js
+++ function.js
@@ -16,3 +16,12 @@ function valor_esconde2() {
     document.getElementById('v2').style.display = 'none';
     document.getElementById('veneno').style.display = 'block';
 }
+function valor_3() {
+    document.getElementById('a').style.display = 'block';
+    document.getElementById('aleatoria').style.display = 'none';
+}
+
+function valor_esconde3() {
+    document.getElementById('a').style.display = 'none';
+    document.getElementById('aleatoria').style.display = 'block';
+}
